5f8550f8827e583b359819ce60c3c3380cda0fae
tly1001 <thomasly1992@gmail.com>
2020-05-15 17:30:11 +0100

    added movable map

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -4,18 +4,27 @@ import MapGl, { Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 class Map extends React.Component {
+  state = {
+    viewport: {
+      latitude: 51.515,
+    longitude: -0.078,
+    width: '80vw',
+    height: '80vh',
+    zoom: 13
+    }
+  }
 
   render() {
+    const { viewport } = this.state
 
     return (
       <MapGl
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        height={'100vh'} //* takes any valid css sizing for the map
-        width={'100vw'}
         mapStyle='mapbox://styles/mapbox/light-v10'
-        latitude={51.515}
-        longitude={-0.078}
-        zoom={13} // * between (very out) 1 - 20 (very in)
+        { ...viewport }
+        onViewportChange= {viewport => {
+          this.setState({ viewport })
+        }}
       >
         <Marker
           latitude={51.515}
