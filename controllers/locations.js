53f2a415aa9d6a992fe957c2c8f64d42c863ac2c
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-05-15 13:22:07 +0100

    added routes for collecting facility information

--- controllers/locations.js
+++ controllers/locations.js
@@ -32,7 +32,7 @@ async function getLocalFacilityData(req, res) {
     const locationData = response.data.results
     const cleanedUpData = locationData.map(location => {
       return ({
-        id: location.id,
+        place_id: location.place_id,
         name: location.name,
         lat: location.geometry.location.lat,
         lng: location.geometry.location.lng,
@@ -51,47 +51,39 @@ async function getLocalFacilityData(req, res) {
 }
 
 
-// -----------------------  GET REQUEST FROM FRONT END ('/locations/id') ------------------------
+// -----------------------  GET ONE LOCATION REQUEST FROM FRONT END ('/locations/places_id') ------------------------
 // ------------- returns ONE location which has been cleaned up to be saved in state -----------
 // REQUIRES A BODY = {
-// keyword: String,
-// radius: number,
-// latitude: number,
-// longitude: number,
+// places_id: String
 // }
 
 async function getOneFacility(req, res) {
-  const googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+  const googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/details/json?'
   const apiKey = 'AIzaSyAmR3drNq7VbhNZTH1e0esR4oTQZrIIoMI'
-  const keyWord = req
-  const radius = req.body.radius
-  const location = `${req.body.lat},${req.body.lon}`
-  const fields = 'formatted_address,name'
+  const placesId = 'ChIJHzTUIOOn2EcRXDi4UZIj2E4'
+  const fields = 'formatted_address,name,business_status,place_id,type,opening_hours,rating,price_level,geometry,review'
+  
   try {
     const response = await axios.get(googlePlacesURL, {
       params: {
         key: apiKey,
-        radius: radius,
-        location: location,
-        keyword: keyWord,
+        places_id: placesId,
         fields: fields
       }
     })
 
-    const locationData = response.data.results
-    const cleanedUpData = locationData.map(location => {
-      return ({
-        id: location.id,
-        name: location.name,
-        lat: location.geometry.location.lat,
-        lng: location.geometry.location.lng,
-        rating: location.rating,
-        ratingAmount: location.user_ratings_total,
-        type: location.types,
-        location: location.vicinity,
-        openingHours: location.opening_hours
-      })
-    })
+    const data = response.data.results
+    const locationObject = {
+      place_id: data.place_id,
+      name: data.name,
+      lat: data.geometry.location.lat,
+      lng: data.geometry.location.lng,
+      rating: data.rating,
+      ratingAmount: data.user_ratings_total,
+      type: data.types,
+      location: data.vicinity,
+      openingHours: data.opening_hours
+    }
 
     res.status(200).json(cleanedUpData)
   } catch (err) {
