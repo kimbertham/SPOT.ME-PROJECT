8e4464dd85e02905bcdeb52a787d5e54fbd2b9f7
Gabriel Goiri <gabrielgoiri@gmail.com>
2020-05-15 18:18:44 +0100

    bulma navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import GymLocations from './components/gym/gymLocations'
+import GymSearch from './components/gym/gymSearch'
 import Map from './components/map/Map'
 import Navbar from './components/common/Navbar'
 
@@ -11,8 +12,8 @@ const App = () => {
       <BrowserRouter>
       <Navbar/>
         <Switch>
-        <Route exact path="/locations" component={GymLocations} />
-
+        <Route path="/locations" component={GymLocations} />
+        <Route path="/locations:id" component={GymSearch} />
         </Switch>
       </BrowserRouter>
       <Map />
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useImperativeHandle } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 
 class Navbar extends React.Component{
   
-  
+    
+
   render() {
     return (
-      <nav>
-        <div>
-            <button><Link to="/locations"></Link>locations</button>
-            <button><Link to="/login">Log In</Link></button>
-            <button><Link to="/register">Sign Up</Link></button>
+      <nav className="navbar is-dark">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item"></Link>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              <Link to="/locations" className="navbar-item">locations</Link>
+              <Link to="/register" className="navbar-item">Register</Link>
+              <Link to="/login" className="navbar-item">Log In</Link>
+              
+            </div>
+          </div>
         </div>
       </nav>
     )
   }
 } 
+
 export default withRouter(Navbar)
