51db434eb468fcc686dc4989ff2d5403bd188bd3
Kimberley Tham <kimbertham@gmail.com>
2020-05-15 14:58:22 +0100

    front end mapping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import gymLocations from './components/gym/gymLocations'
 
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+
+      <BrowserRouter>
+      <Switch>
+      <Route path='/gym' compoments={gymLocations} />
+      </Switch>
+      </BrowserRouter>
   )
 }
 
