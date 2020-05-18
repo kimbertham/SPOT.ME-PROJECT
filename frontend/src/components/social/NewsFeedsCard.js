4e2d2842f1f36293b42614623417b20c3787ade0
Gabriel Goiri <gabrielgoiri@gmail.com>
2020-05-18 14:57:41 +0100

    styling and harcoding the newsFeed

--- frontend/src/components/social/NewsFeedsCard.js
+++ frontend/src/components/social/NewsFeedsCard.js
@@ -1,23 +1,31 @@
 import React from 'react'
 
 const NewsFeedsCard = (posts, user) => (
-  // * POST { content, user, likes, img (not nbefe) }
+  // * POST { content, user, likes, img }
   // * USERS { name, img }
+  // ? we need in feeds-header the img of the user (user.img)
+  // ? we need in feeds-header-title the user name (user.name)
+  // ? we need in feeds-content the content of the post (post.content)
+  // ? if the user upload a photo we need in feeds-image the photo (post.img)
 
-
-  <div className="container">
+  // ! we need at the button a function to increase the number of likes
+  <div className="wrap-center">
     <div className="feeds-container">
       <div className="feeds-header">
-        <img src={user.img} alt="test" /> 
-        <h4 className="feeds-header-title">{user.posts}</h4>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/440px-Terry_Crews_by_Gage_Skidmore_5.jpg" alt="test" />
+        <h4 className="feeds-header-title">Terry Crews</h4> 
       </div>
       <div className="feeds-content">
-        <h2>{posts.content}</h2>
+        <h1>Lorem ipsum dolor</h1>
       </div>
-      <div>
+      <div className="feeds-image">
         <figure>
-          <img src="test.jpg" alt="test"/>
+          <img src="https://previews.123rf.com/images/nexusplexus/nexusplexus1309/nexusplexus130901873/22071838-imagen-de-libro-m%C3%A1gico-abierto-con-luces-m%C3%A1gicas.jpg" alt="test"/> 
         </figure>
+      </div>
+      <div className="feeds-likes">
+        <p><span>10 </span>likes</p>
+        <button className="like">Like!</button>
       </div>
     </div>
   </div>
--- frontend/src/components/social/Profile.js
+++ frontend/src/components/social/Profile.js
@@ -25,7 +25,6 @@ async componentDidMount() {
         user={this.state.user}
         />
         <NewsFeedsCard
-        user={this.state}
         />
       </div>
     )
