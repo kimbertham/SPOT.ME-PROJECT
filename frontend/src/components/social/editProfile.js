734c35a17b388827a0d07205e0bd5e00841cde46
Kimberley Tham <kimbertham@gmail.com>
2020-05-18 17:04:07 +0100

    fix

--- frontend/src/components/social/editProfile.js
+++ frontend/src/components/social/editProfile.js
@@ -5,9 +5,9 @@ import axios from 'axios'
 class editProfile extends React.Component {
 state = { 
   data: {
-  description: '',
-  firstName: '',
-  lastName:'',
+    description: '',
+    firstName: '',
+    lastName: ''
   }
 }
 
@@ -22,16 +22,16 @@ async componentDidMount() {
 }
 
  handleSubmit = async event => {
-  event.preventDefault()
-  const userId = this.props.match.params.userId
-  try {
-  const data = {...this.state.data}
-  await axios.post(`/api/profile/${userId}/edit`, data)
-  this.props.history.push(`/profile/${userId}`)
-} catch (err) {
-  console.log(err)
-}
-}
+   event.preventDefault()
+   const userId = this.props.match.params.userId
+   try {
+    const data = { ...this.state.data }
+    await axios.post(`/api/profile/${userId}/edit`, data)
+    this.props.history.push(`/profile/${userId}`)
+   } catch (err) {
+    console.log(err)
+   }
+ }
 
 
 handleChange = event => {
@@ -45,42 +45,42 @@ render() {
   return (
     <div className='edit-profile-container'>
     
-  <div className='edit-form-container'> 
-    <form onSubmit={this.handleSubmit} className="edit-profile-form">
-      <div className="edit-field">
-        <label className="label"> First Name </label>
-          <input 
-          className='edit-input'
-            placeholder="First Name here"
-            name="firstName"
-            onChange={this.handleChange}
-            value={this.state.data.firstName}
-          />
-        </div>
+      <div className='edit-form-container'> 
+        <form onSubmit={this.handleSubmit} className="edit-profile-form">
+          <div className="edit-field">
+            <label className="label"> First Name </label>
+            <input 
+              className='edit-input'
+              placeholder="First Name here"
+              name="firstName"
+              onChange={this.handleChange}
+              value={this.state.data.firstName}
+            />
+          </div>
 
-        <div className="edit-field">
-        <label className="label"> Last Name</label>
-          <input 
-          className='edit-input'
-            placeholder="last Name"
-            name="lastName"
-            onChange={this.handleChange}
-            value={this.state.data.lastName}
-          />
-        </div>
+          <div className="edit-field">
+            <label className="label"> Last Name</label>
+            <input 
+              className='edit-input'
+              placeholder="last Name"
+              name="lastName"
+              onChange={this.handleChange}
+              value={this.state.data.lastName}
+            />
+          </div>
 
-        <div className="edit-field">
-        <label className="label">Description </label>
-          <textarea 
-            className='edit-textarea'
-            placeholder="description here"
-            name="description"
-            onChange={this.handleChange}
-            value={this.state.data.description}
-          />
-        </div>
+          <div className="edit-field">
+            <label className="label">Description </label>
+            <textarea 
+              className='edit-textarea'
+              placeholder="description here"
+              name="description"
+              onChange={this.handleChange}
+              value={this.state.data.description}
+            />
+          </div>
 
-        {/* <div className="edit-field">
+          {/* <div className="edit-field">
         <label className="label"> Level</label>
           <textarea 
             placeholder="level here"
@@ -89,12 +89,12 @@ render() {
             value={this.state.data.level}
           />
         </div> */}
-        <button>send!</button>
+          <button>send!</button>
         </form>
-        </div>
+      </div>
 
 
-        </div>
+    </div>
   )
 }
 }
