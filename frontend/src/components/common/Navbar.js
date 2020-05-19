6fdcab1dca16cfc52091a15a2ccaf56b8584dc15
tly1001 <thomasly1992@gmail.com>
2020-05-19 10:36:33 +0100

    ready merge

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -23,19 +23,11 @@ class Navbar extends React.Component{
           </div>
           <div className="navbar-menu">
             <div className="navbar-end">
-<<<<<<< HEAD
-              <Link to="/locations" className="navbar-item">locations</Link>
-              <Link to="/register" className="navbar-item">Register</Link>
-              <Link to="/login" className="navbar-item">Log In</Link>
-              <Link to={`/profile/${getUserId()}`} className="navbar-item">Profile</Link>
-              <Link to="/home" className="navbar-item">Home</Link>
-=======
             {isAuthenticated() && <Link to="/locations" className="navbar-item">locations</Link>}
             {!isAuthenticated() && <Link to="/register" className="navbar-item">Register</Link>}
             {!isAuthenticated() && <Link to="/login" className="navbar-item">Log In</Link>}
             {isAuthenticated() && <Link to={`/profile/${getUserId()}`} className="navbar-item">Profile</Link>}
             {isAuthenticated() && <span onClick={this.handleLogout} className="navbar-item">Logout</span>}
->>>>>>> seeds
             </div>
           </div>
         </div>
