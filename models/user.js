fed0a6d8f1df45f8734164c51405911bf629023c
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-05-19 11:01:08 +0100

    model cleaning

--- models/user.js
+++ models/user.js
@@ -1,23 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-// const Group = require('./Group') //* not in use yet
-
-const commentSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
-}, {
-  timestamps: true
-})
-
-const postSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  likes: [{ type: mongoose.Schema.ObjectId, ref: 'User', required: true }],
-  comments: [commentSchema]
-}, {
-  timestamps: true
-})
-
+const postSchema = require('./postSchema')
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, maxlength: 50 },
