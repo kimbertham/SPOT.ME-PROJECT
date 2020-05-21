261be6df3073704fbc829245ad0209691800fa84
tly1001 <thomasly1992@gmail.com>
2020-05-21 13:13:13 +0100

    old

--- frontend/src/components/social/Profile.js
+++ frontend/src/components/social/Profile.js
@@ -1,115 +1,98 @@
 import React from 'react' 
 import Post from './Post'
 import axios from 'axios'
+import { getProfile } from '../../lib/api'
 import ProfileInfo from '../social/ProfileInfo'
+import ProfileSidebar from '../common/ProfileSidebar'
 import FriendsSidebar from '../common/FriendsSidebar'
 import NewsFeedsCard from './NewsFeedsCard'
-import ProfileSidebar from './ProfileSidebar'
-import { getProfile, getLike, commentADelete, deleteAPost, postAComment } from '../../lib/api'
+import { withHeaders } from '../../lib/api'
 import { getUserId } from '../../lib/auth'
-
-
 
 class Profile extends React.Component {
 state = {
-  user: {}, 
-  data: {
+  user: {},
+  modal: false,
+  data : {
     content: ''
   },
-  modal: false,
-  index: null,
-  postsUser: {}
 }
 
-async componentDidMount() {
+getData = async () => {
   const userId = this.props.match.params.userId
   const res = await getProfile(userId)
   this.setState( { user: res.data }) 
+}
+
+async componentDidMount() {
   try {
+    this.getData() 
   } catch (err) {
     console.log(err)
   }
 }
 
 addLike = async (postId) => {
-  const userId = this.props.match.params.userId
-  await getLike(userId, postId)
-  const res = await getProfile(userId)
-  this.setState({  user :res.data  }, () => { console.log(this.state.user)})   
+  try {
+    const userId = getUserId()
+    const res = await axios.put(`/api/profile/${userId}/post/${postId}`,'' ,  withHeaders() )
+    
+    this.setState( { user: res.data }) 
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 handleChange = event => {
-  const data = 
-  { ...this.state.data, [event.target.name]: event.target.value }
-  this.setState( { data } )
+  const data = { ...this.state.data, [event.target.name]: event.target.value }
+  this.setState({ data })
 }
 
-postComment = async ( postOwner, postId) =>{
-  const content = this.state.data
-  await postAComment(postOwner,postId, content)
-  const userId = this.props.match.params.userId
-  const res = await getProfile(userId)
-  this.setState( { user: res.data })  
-}
-
-commentDelete = async (postId, commentId) => {
-  const userId=getUserId()
-  commentADelete( userId ,postId,commentId)
-  const res = await getProfile(userId)
-  this.setState( { user: res.data })  
-}
-
-deletePost = async (postId) => {
+  postAComment = async (postOwner, postId) => {
+    console.log(this.state)
   const userId = getUserId()
-deleteAPost(userId, postId)
-  const res = await getProfile(userId)
-  this.setState( { user: res.data })  
+  const res = await axios.put(`/api/profile/${postOwner}/post/${postId}/comment`, this.state.data , withHeaders() )
+  const content = res.data
 }
 
-setIndex = async (i) => {
-  await this.setState({index: i})
+setModal =() => {
+  this.setState({ modal: true })
+}
+hideModal = () => {
+  this.setState({ modal: false })
 }
 
 render(){
-  const { user } = this.state
-  const posts = user.posts ? user.posts : []
-  console.log(this.state)
+  // console.log(this.state)
+  // console.log(this.props)
   return (
     <div className='profile-page-container'>
         
-        <ProfileSidebar 
-            modal={this.state.modal}
-            setModal={this.setModal}
-            hideModal={this.hideModal}
-            user={this.state.user.id}/>
+      <ProfileSidebar 
+        modal={this.state.modal}
+        setModal={this.setModal}
+        hideModal={this.hideModal}
+        user={this.state.user.id}/>
 
       <div className='mid-section'>
+
         <div className='profile-info-component'>
           <ProfileInfo 
             user={this.state.user}/>
         </div>
+
         <div className='profile-post'>
           <Post 
             user={this.state.user}
             refresh={this.getData}
           />
 
-      {posts.slice(0).reverse().map((post, i) => {
-          return <NewsFeedsCard
-            post={post}
+          <NewsFeedsCard
             user={this.state.user}
             like={this.addLike}
-            comment={this.postComment}
+            comment={this.postAComment}
             change={this.handleChange}
-            key={`profile${post._id}`}
-            commentDelete={this.commentDelete}
-            deletePost={this.deletePost}
-            showLikes={this.showLikes}
-            setIndex={this.setIndex}
-            i={i}
-            indexState={this.state.index}
           />
-        })}
 
 
         </div>
