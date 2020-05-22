ae8003e975cee9a237dbe925d66dc12de8f7f472
tly1001 <thomasly1992@gmail.com>
2020-05-22 10:57:20 +0100

    kim pulled

--- frontend/src/components/social/ProfileInfo.js
+++ frontend/src/components/social/ProfileInfo.js
@@ -17,7 +17,7 @@ class ProfileInfo extends React.Component {
     followUser = async () =>{
       const userId = this.props.user.id
       const message = await followAUser(userId)
-      notify(message.data)
+      // notify(message.data)
     // this.setState({ showFollowers: !this.state.showFollowers })
   }
 
